5b4b6de71e78b053ab9ff57a55611fd86d4a77a7
Krish-in-real <89638684+Krish-in-real@users.noreply.github.com>
2025-08-10 08:47:35 +0530

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Chats from "./pages/Chats";
 import ChatPage from "./pages/ChatPage";
 
